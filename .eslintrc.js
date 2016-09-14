9566f192d98f7f5eee85bb68b91281947370083c
Oliver Buchtala <oliver.buchtala@gmail.com>
2016-09-14 17:28:38 +0200

    Changed eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -129,6 +129,6 @@ module.exports = {
     "no-undef-init": 2,
     "no-undefined": 0,
     "no-unused-vars": 2,
-    "no-use-before-define": [2, { "functions": false }]
+    "no-use-before-define": [2, { "functions": false, "classes": false }]
   }
 };
